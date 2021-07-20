faa82aa161898f69b32ec1bb4466deeff6e901df
Lou <83348079+LucisLou@users.noreply.github.com>
2021-07-20 09:45:36 +0200

    Create FreeTimeOrWorkTimeSwitch.java
    Initial

---
+++ FreeTimeOrWorkTimeSwitch.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class FreeTimeOrWorkTimeSwitch {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Hey, what time is it now?%nGive the time in the 24h format in full integers (e.g. ... 9 10 ...):%n");
+
+        int time = scanner.nextInt();
+
+        while (time >= 25 || time <= 0) {
+            System.out.printf("%d is not a valid input. Please insert the time in the 24h format in full integers (e.g. ... 9 10 ...):%n", time);
+            time = scanner.nextInt();
+        } //Runs while loop until user inserts a number from 1 to 24 to avoid a wrong input.
+
+        String isWorkTime = switch (time) {
+            case 8, 9, 10, 11, 14, 15, 16:
+                yield "It's still work time!";
+            case 12:
+                yield "It's lunch break!";
+            case 13:
+                yield "Lunch break just ended.. Back to work!";
+            case 1, 2, 3, 4, 5, 6, 7, 17, 18, 19, 20, 21, 22, 23, 24:
+                yield "It's not work time! Hooray!";
+            default:
+                yield "invalid";
+        };
+
+        System.out.printf("It's %d o'clock! Which means: %s", time, isWorkTime);
+    }
+}
